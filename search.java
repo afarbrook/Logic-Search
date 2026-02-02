4b86b9be5674fdbc80c5db2f16eaaaa397ae9d3b
Alexander Farbrook <AFarbro1@CITY.TUCSONAZ.GOV>
2026-02-02 15:19:37 -0700

    second change

--- search.java
+++ search.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class search{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        if(args.length <1){
+        if(args.length <0){
             System.out.println("Please provide a file name as a command line argument.");
             System.exit(1);
         }
-        String fileName = args[0];
+        String fileName = "program_1_example_input.txt";//args[0];change later
         String type = in.nextLine();
-        ArrayList<String> RTs = new ArrayList<>(); //stores all the search terms
+        searchMap RTs = new searchMap(); //stores all the search terms
         while(in.hasNextLine()){
-            String line = in.nextLine();
-            RTs.add(line);
+            String[] parts = in.nextLine().split(" ");
+            if(RTs)
         }
         
     }
--- searchMap.java
+++ searchMap.java
@@ -1,8 +1,25 @@
+import java.util.*;
+
 public class searchMap {
-    
 
+    private HashSet<Node> searched;
 
-    [public searchMap()]
+    public searchMap(){
+        searched = new HashSet<>();
+    }
+
+    public boolean isSearched(Node pos){
+        return searched.contains(pos);
+    }
+
+    public void mark(Node pos){
+        searched.add(pos);
+    }
+
+    public boolean isValid(int x, int y){
+        return (x>-1 && x<50) && (y>-1 && y<50);
+    }
+
 }   
 
     
